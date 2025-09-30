0ce6c07c7a669f69a0331c8e215f85f017c0620a
algo9311 <ferbalboam@icloud.com>
2025-09-29 19:48:59 -0600

    Add files via upload

--- ping-pong.py
+++ ping-pong.py
@@ -14,6 +14,32 @@ finish = False
 clock = time.Clock()
 FPS = 60
 
+class GameSprite(sprite.Sprite):
+    def __init__(self,player_image, player_x, player_y, player_speed, wight, height):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (wight, height))
+        self.speed = player_speed
+        self.rect = self.image.gect_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+
+    def reset (self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys [K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+    def update_1(self):
+        keys = key.get_pressed()
+        if keys [K_W] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys [K_S] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
 
 while game:
     for e in event.get():
